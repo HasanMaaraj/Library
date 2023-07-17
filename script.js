9fd7740500c58a6107bfe4888b94811b81493059
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2023-07-17 19:38:55 +0300

    Add comments

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ Book.prototype.changeStatus = function() {
 const myBook = new Book('my book', 'hasan', 100, 'Read')
 myLibrary.push(myBook)
 
+// Display all books in the array
 for(let i=0; i < myLibrary.length; i++) {
     addBookToLibrary(myLibrary[i], i);
 }
@@ -25,22 +26,27 @@ for(let i=0; i < myLibrary.length; i++) {
 
 
 function addBookToLibrary(book, index) {
+    // Define the card container for the book information
     bookCard = document.createElement('div');
     bookCard.className = 'book-card';
     bookCard.dataset.book = index;
+    // Define and append the title for the book
     title = document.createElement('h3')
     title.textContent = `Title: ${book.title}`;
     title.className = 'book-title';
     bookCard.appendChild(title);
+    // Define and append the author of the book
     author = document.createElement('h4')
     author.textContent = `Author: ${book.author}`;
     author.className = 'book-author';
     bookCard.appendChild(author);
+    // Define and append the pages of the book
     pages = document.createElement('p')
     pages.textContent = `Pages: ${book.pages}`;
     pages.className = 'book-title';
     bookCard.appendChild(pages);
     
+    // Define and append the read status button
     const statusButton = document.createElement('button');
     statusButton.className = 'status-button';
     statusButton.dataset.book = index;
@@ -51,6 +57,7 @@ function addBookToLibrary(book, index) {
     })
     bookCard.appendChild(statusButton)
 
+    // Define and append the delete button for the book
     const deleteButton = document.createElement('button')
     deleteButton.className = 'delete-button'
     deleteButton.textContent = 'Delete';
@@ -78,9 +85,11 @@ form.addEventListener('submit', (e) => {
     const author = form.querySelector('input[name="author"]').value;
     const pages = parseInt(form.querySelector('input[name="pages"]').value);
     const status = form.querySelector('select[name="status"]').value;
+    // Clear form fields
     form.querySelector('input[name="title"]').value = '';
     form.querySelector('input[name="author"]').value = '';
     form.querySelector('input[name="pages"]').value = '';
+    // Create book object and append it to the array
     newBook = new Book(title, author, pages, status)
     myLibrary.push(newBook)
     addBookToLibrary(newBook, myLibrary.length-1)
