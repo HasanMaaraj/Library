ebd72b38527c6389ff65dda256ed635b28c45a47
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2023-07-17 17:09:29 +0300

    Create addBookToLibrary function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let myLibrrary = [];
+let myLibrary = [];
 
 function Book(title, author, pages, status) {
     this.title = title;
@@ -7,7 +7,46 @@ function Book(title, author, pages, status) {
     this.status = status;
 }
 
+const myBook = new Book('my book', 'hasan', 6, 'Not Read')
+addBookToLibrary(myBook, 0)
 
+function addBookToLibrary(book, index) {
+    bookCard = document.createElement('div');
+    bookCard.className = 'book-card';
+    bookCard.dataset.book = index;
+    title = document.createElement('h3')
+    title.textContent = `Title: ${book.title}`;
+    title.className = 'book-title';
+    bookCard.appendChild(title);
+    author = document.createElement('h4')
+    author.textContent = `Author: ${book.author}`;
+    author.className = 'book-author';
+    bookCard.appendChild(author);
+    pages = document.createElement('p')
+    pages.textContent = `Pages: ${book.pages}`;
+    pages.className = 'book-title';
+    bookCard.appendChild(pages);
+    
+    const statusButton = document.createElement('button');
+    statusButton.className = 'status-button';
+    statusButton.dataset.book = index;
+    statusButton.textContent = book.status;
+    statusButton.addEventListener('click', () => {
+        // changeStatus(index);
+        statusButton.textContent = myLibrary[index].status;
+    })
+    bookCard.appendChild(statusButton)
+
+    const deleteButton = document.createElement('button')
+    deleteButton.textContent = 'Delete';
+    deleteButton.dataset.book = index;
+    deleteButton.addEventListener('click', () => {
+        // deleteBook(index)
+    })
+    bookCard.appendChild(deleteButton)
+
+    document.getElementById('library').appendChild(bookCard)
+}
 
 const form = document.getElementById('new-book-form')
 form.addEventListener('submit', (e) => {
@@ -17,5 +56,5 @@ form.addEventListener('submit', (e) => {
     const pages = parseInt(form.querySelector('input[name="pages"]').value);
     const status = form.querySelector('select[name="status"]').value;
     newBook = Book(title, author, pages, status)
-    myLibrrary.push(newBook)
+    myLibrary.push(newBook)
 })
